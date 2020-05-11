32bdcb9bd27de5a6a1e2b1b456d1c2485452e4f9
shridhargiri <shri.v.giri@gmail.com>
2020-05-11 19:02:51 +0530

    DAY1 Practical Test submission

---
+++ Day1/js/main.js
@@ -0,0 +1,46 @@
+/*---------Highest Number----------*/
+function highest()
+{
+    var a = document.getElementById("num1").value;
+    var b = document.getElementById("num2").value;
+    var c = document.getElementById("num3").value;
+    var max = 0;
+    if(a>max){max = a;}
+    if(b>max){max = b;}
+    if(c>max){max = c;}
+    window.alert(max);
+}
+/*---------Display name---------*/
+function nameInBrowser()
+{
+    var name=document.getElementById("inName").value;
+    document.getElementById("name").innerHTML=name;
+}
+function nameOnConsole()
+{
+    var name=document.getElementById("inName").value;
+    console.log(name);
+}
+function nameAsAlert()
+{
+    var name=document.getElementById("inName").value;
+    window.alert(name);
+}
+/*---------Name in different headings---------*/
+function nameH1()
+{
+    document.getElementById("nameInHeads").innerHTML="<h1>Shridhar Giri</h1>";
+}
+function nameH2()
+{
+    document.getElementById("nameInHeads").innerHTML="<h2>Shridhar Giri</h2>";
+}function nameH3()
+{
+    document.getElementById("nameInHeads").innerHTML="<h3>Shridhar Giri</h3>";
+}function nameH4()
+{
+    document.getElementById("nameInHeads").innerHTML="<h4>Shridhar Giri</h4>";
+}function nameH5()
+{
+    document.getElementById("nameInHeads").innerHTML="<h5>Shridhar Giri</h5>";
+}
