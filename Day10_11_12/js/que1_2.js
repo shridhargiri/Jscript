7874a97bca1fd02847acafa9cd9292c5bd1e3355
shridhargiri <shri.v.giri@gmail.com>
2020-05-30 00:07:44 +0530

    Day 10-11-12 part-1

---
+++ Day10_11_12/js/que1_2.js
@@ -0,0 +1,7 @@
+function changeColor(){
+    var name = document.getElementById("name");
+    name.style.color = "blue";
+}
+function changeAlignm(){
+    document.getElementById("h1name").style.textAlign = "right";
+}
